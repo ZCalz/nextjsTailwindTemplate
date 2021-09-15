d0be6f5bec4ec876f6a3ea1a9a85f29d43797010
Calman <csze4107@gmail.com>
2021-09-15 01:16:28 -0400

    updated purchasing capability

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,11 @@
 import Image from "next/image";
+// import { MakePixelz } from "../../src/pixelz.js";
 
 export default function IndexPage() {
+  // async function adoptPixelz(num) {
+  //    const pixelz = await MakePixelz()
+  //    await pixelz.purchasePixelzNFT(num)
+  // }
   return (
     <div className="flex flex-col items-center justify-center space-y-12">
       <Image
@@ -10,7 +15,11 @@ export default function IndexPage() {
         height={429.734}
         priority
       />
-
+      <div>
+        {/* <button onClick={adoptPixelz} className="font-bold mt-4 bg-purple-600 text-white rounded p-4 shadow-lg">
+          Adopt a Pixel
+        </button> */}
+      </div>
       <h2 className="p-3 font-bold bg-yellow-300 md:text-2xl">
         Hi! Welcome to your first Next.js site.
       </h2>
